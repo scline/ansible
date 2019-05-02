2e123d5cfdda31ff9b266b7852f31b9cb7359e9b
Sean Cline <scline@riotgames.com>
2019-05-01 17:47:38 -0700

    WIP for minecraft map role

---
+++ roles/minecraft-map/files/config.py
@@ -0,0 +1,82 @@
+# https://github.com/mide/minecraft-overviewer/blob/master/config/config.py
+import os
+
+
+def playerIcons(poi):
+    if poi['id'] == 'Player':
+        poi['icon'] = "https://overviewer.org/avatar/{}".format(poi['EntityId'])
+        return "Last known location for {}".format(poi['EntityId'])
+
+
+# Only signs with "-- RENDER --" in them, and no others. Otherwise, people
+# can't have secret bases and the render is too busy anyways.
+def signFilter(poi):
+    if poi['id'] in ['Sign', 'minecraft:sign']:
+        if '-- RENDER --' in poi.values():
+            return "\n".join([poi['Text1'], poi['Text2'], poi['Text3'], poi['Text4']])
+
+
+worlds['minecraft'] = "/home/minecraft/server/world"
+outputdir = "/home/minecraft/render/"
+texturepath = "/home/minecraft/{}.jar".format(os.environ['MINECRAFT_VERSION'])
+
+markers = [
+    dict(name="Players", filterFunction=playerIcons),
+    dict(name="Signs", filterFunction=signFilter)
+]
+
+renders["day"] = {
+    'world': 'minecraft',
+    'title': 'Day',
+    'rendermode': 'smooth_lighting',
+    "dimension": "overworld",
+    'markers': markers
+}
+
+renders["night"] = {
+    'world': 'minecraft',
+    'title': 'Night',
+    'rendermode': 'smooth_night',
+    "dimension": "overworld",
+    'markers': markers
+}
+
+renders["nether"] = {
+    "world": "minecraft",
+    "title": "Nether",
+    "rendermode": 'nether_smooth_lighting',
+    "dimension": "nether",
+    'markers': markers
+}
+
+renders["end"] = {
+    "world": "minecraft",
+    "title": "End",
+    "rendermode": [Base(), EdgeLines(), SmoothLighting(strength=0.5)],
+    "dimension": "end",
+    'markers': markers
+}
+
+renders["overlay_biome"] = {
+    'world': 'minecraft',
+    'rendermode': [ClearBase(), BiomeOverlay()],
+    'title': "Biome Coloring Overlay",
+    "dimension": "overworld",
+    'overlay': ["day"]
+}
+
+#renders["overlay_mobs"] = {
+#    'world': 'minecraft',
+#    'rendermode': [ClearBase(), SpawnOverlay()],
+#    'title': "Mob Spawnable Areas Overlay",
+#    "dimension": "overworld",
+#    'overlay': ["day"]
+#}
+#
+#renders["overlay_slime"] = {
+#    'world': 'minecraft',
+#    'rendermode': [ClearBase(), SlimeOverlay()],
+#    'title': "Slime Chunk Overlay",
+#    "dimension": "overworld",
+#    'overlay': ["day"]
+#}
---
+++ roles/minecraft-map/files/sync.sh
@@ -0,0 +1,37 @@
+#/usr/bin/bash
+
+# Bash script that pulls most recent Minecraft Databackups and extract the world information for further processing.
+# Working Directory = /var/minecraft-map
+
+#Vanilla Variables
+v_www_output="/var/minecraft-map/vanilla-www"
+v_zip_output="/var/minecraft-map/vanilla-raw"
+v_zip_input=`ls -t /mnt/nfs/darkwindcraft/Vanilla/Databackup/*.zip | head -1`
+
+# Pixelmon Variables
+p_www_output="/var/minecraft-map/pixelmon-www"
+p_zip_output="/var/minecraft-map/pixelmon-raw"
+p_zip_input=`ls -t /mnt/nfs/darkwindcraft/Pixelmon/Databackup/*.zip | head -1`
+
+# unzip vanilla
+echo "Decompresing $v_zip_input to $v_zip_output"
+unzip -qou $v_zip_input -d $v_zip_output
+
+# unzip pixelmon
+echo "Decompresing $p_zip_input to $p_zip_output"
+unzip -qou $p_zip_input -d $p_zip_output
+
+# Fixing permissions
+chmod 777 $p_www_output
+chmod 777 $v_www_output
+
+# Pull latest docker image
+docker pull mide/minecraft-overviewer:latest
+
+# Run container for rendering Vanilla
+docker run --rm -d \
+-e MINECRAFT_VERSION="1.14" \
+-v /etc/docker/compose/minecraft-map/config.py:/home/minecraft/config.py:ro \
+-v $v_zip_output:/home/minecraft/server/:ro \
+-v $v_www_output:/home/minecraft/render/:rw \
+mide/minecraft-overviewer:latest
